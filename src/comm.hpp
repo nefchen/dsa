585d1d6957091b42fd6b00266255f98fc03b2097
nef-chan <nef-chan@mailbox.org>
2021-01-24 18:59:04 +0100

    Added mouse and window signals

--- src/comm.hpp
+++ src/comm.hpp
@@ -8,6 +8,8 @@
 #include <memory>
 
 #include "signals.hpp"
+#include "user_input.hpp"
+#include "types.hpp"
 
 
 namespace comm
@@ -15,10 +17,26 @@ namespace comm
     struct AppControlNode
     {
         AppControlNode(std::shared_ptr<Dispatcher> dispatcher)
-            : app_exit_request{dispatcher}
+            : app_exit_request{dispatcher},
+              create_window_request{dispatcher}
         {};
 
         Signal<> app_exit_request;
+        Signal<std::shared_ptr<AppControlNode>> create_window_request;
+        Signal<Id> destroy_window_request;
+    };
+
+    struct WindowNode
+    {
+        WindowNode(std::shared_ptr<Dispatcher> dispatcher)
+            : mouse_button_pressed_released{dispatcher},
+              mouse_button_click{dispatcher},
+              mouse_moved{dispatcher}
+        {};
+
+        Signal<mouse::Button, mouse::ButtonState> mouse_button_pressed_released;
+        Signal<mouse::Button, u8> mouse_button_click;
+        Signal<Point> mouse_moved;
     };
 }
 
