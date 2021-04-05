ba8f429e6966f32a8cbbe291fdeecfcb15707481
nefchen <nefchen@mailbox.org>
2021-04-05 18:54:50 +0200

    Implemented add_entity_to_simulation

--- src/game/simulation/simulation.cpp
+++ src/game/simulation/simulation.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "game/simulation/simulation.hpp"
+#include "game/simulation/positioning.hpp"
 
 
 namespace game
@@ -10,10 +11,37 @@ namespace game
     void Simulation::reset_to_initial_state()
     {};
 
+    // Tries placing entity in simulation world anywhere within
+    // the designated spawn_area.
+    // NOTE: different placement implementations may yield
+    // different results for the same configuration.
     bool Simulation::add_entity_to_simulation(
-        std::unique_ptr<Entity> const& entity, bool relax_spawn_point)
+        std::unique_ptr<Entity> const& entity, Rect spawn_area)
     {
-        return false;
+        auto entity_physics{create_entity_physics(entity)};
+        auto collision_targets{find_entities_in_rect(spawn_area)};
+
+        if (!place_entity_with_naive_spiral(spawn_area, entity_physics, collision_targets))
+        {
+            // No place found to put new entity.
+            return false;
+        }
+
+        m_entities.push_back(std::move(entity_physics));
+
+        return true;
+    };
+
+    std::vector<EntityPhysics*> Simulation::find_entities_in_rect(Rect rect)
+    {
+        std::vector<EntityPhysics*> entities_in_rect;
+
+        for (auto& entity: m_entities)
+        {
+            entities_in_rect.push_back(&entity);
+        }
+
+        return entities_in_rect;
     };
 }
 
